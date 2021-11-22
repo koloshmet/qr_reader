f0a538e86ea9098420fdf50263c8f353ae698106
koloshmet <mvguzov@edu.hse.ru>
2021-11-22 20:54:12 +0300

    Install script

---
+++ install_mac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check and install brew
+
+if (which -s brew)
+then
+    echo 'Brew already installed'
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && echo 'Brew installed'
+fi
+
+# Install required system packages
+brew install python3 tcl-tk python-tk zbar
+
+# Install required python packages to local environment
+python3 -m ensurepip --upgrade
+mkdir venv && python3 -m venv venv
+./venv/bin/python3 -m pip install -r requirements.txt
+
+# Exporting local environment
+export PATH=./venv/bin/:$PATH
+
