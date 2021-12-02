ac38ef2fc042721b4416c079396e1b711cf9aff5
koloshmet <mvguzov@edu.hse.ru>
2021-12-02 12:40:49 +0300

    Improved mac installer

--- install_mac.sh
+++ install_mac.sh
@@ -11,9 +11,14 @@ fi
 
 # Install required system packages
 brew install python3 tcl-tk python-tk zbar
+brew upgrade
 
 # Install required python packages to local environment
-/usr/local/bin/python3 -m ensurepip --upgrade
-mkdir venv && /usr/local/bin/python3 -m venv venv
+BREW_PYTHON=$(brew --prefix python3)/bin/python3
+
+$BREW_PYTHON -m ensurepip --upgrade
+rm -rf venv
+mkdir venv
+$BREW_PYTHON -m venv venv
 ./venv/bin/python3 -m pip install -r requirements.txt
 
